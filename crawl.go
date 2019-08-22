b0febf926add294174d9327481051e609aa4a170
thisisaaronland <devnull@localhost>
2019-08-22 15:47:36 -0700

    implement CrawlWithContextAndRequestHandler

--- crawl.go
+++ crawl.go
@@ -14,6 +14,8 @@ type ProcessingRequest struct {
 
 type CrawlFunc func(path string, info os.FileInfo) error
 
+type RequestHandlerFunc func(req *ProcessingRequest) bool
+
 type Crawler struct {
 	Root             string
 	CrawlDirectories bool
@@ -29,11 +31,19 @@ func NewCrawler(path string) *Crawler {
 func (c Crawler) Crawl(cb CrawlFunc) error {
 
 	ctx := context.Background()
-
 	return c.CrawlWithContext(ctx, cb)
 }
 
 func (c Crawler) CrawlWithContext(ctx context.Context, cb CrawlFunc) error {
+
+	req_handler := func(_ *ProcessingRequest) bool {
+		return true
+	}
+
+	return c.CrawlWithContextAndRequestHandler(ctx, cb, req_handler)
+}
+
+func (c Crawler) CrawlWithContextAndRequestHandler(ctx context.Context, cb CrawlFunc, req_handler RequestHandlerFunc) error {
 
 	// this bit is important - see abouts about ctx.Done() and DoneError()
 	// below in CrawlWithChannels (20190822/thisisaaronland)
@@ -52,7 +62,7 @@ func (c Crawler) CrawlWithContext(ctx context.Context, cb CrawlFunc) error {
 		case <-done_ch:
 			return nil
 		case req := <-processing_ch:
-			req.Ready <- true
+			req.Ready <- req_handler(req)
 		case err := <-error_ch:
 			return err
 		}
@@ -88,23 +98,40 @@ func (c Crawler) CrawlWithChannels(ctx context.Context, cb CrawlFunc, processing
 			return nil
 		}
 
+		// processing request allows people using this package
+		// implement their own custom throttling - here we send
+		// the "processing" channel a request that contains a local
+		// "ready" channel and then we wait for a response. If
+		// the response is true then we carry on but if it's false
+		// we exit out of the function with a nil return value
+
 		ready_ch := make(chan bool)
 		ready := false
 
-		processing_ch <- &ProcessingRequest{Path: path, Ready: ready_ch}
+		req := &ProcessingRequest{
+			Path:  path,
+			Ready: ready_ch,
+		}
+
+		processing_ch <- req
 
 		for {
 			select {
-			case <-ready_ch:
+			case ready_value := <-ready_ch:
+
+				if ready_value == false {
+					return nil
+				}
+
 				ready = true
-			default:
-				// log.Println("WAITING")
 			}
 
 			if ready {
 				break
 			}
 		}
+
+		// okay, carry on
 
 		err = cb(path, info)
 
